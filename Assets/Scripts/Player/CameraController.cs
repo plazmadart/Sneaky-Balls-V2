190451187b3cab2419c157f16e2a605db4f7bec9
skrininshot <38591071+skrininshot@users.noreply.github.com>
2022-07-16 18:07:38 +0500

    Update Camera

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -6,10 +6,21 @@ public class CameraController : MonoBehaviour
     [SerializeField][Range(1f, 5f)] private float distance = 3f;
     [SerializeField][Range(0.5f, 5f)] private float height = 0.7f;
     [SerializeField][Range(0f, 1f)] private float moveSpeed = 0.125f;
+    [SerializeField][Range(0f, 25f)] private float angleClamp = 15f;
+    [SerializeField][Range(0f, 25f)] private float sensitivity = 5f;
 
+    private float angle = 0;
     private void Start()
     {
         target.transform.rotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        //Нужно поменять мышь на палец
+        angle = Mathf.Clamp(angle - Input.GetAxis("Mouse Y") * sensitivity, -angleClamp, angleClamp);
+        target.transform.eulerAngles = new Vector3(
+            angle, target.transform.eulerAngles.y + Input.GetAxis("Mouse X") * sensitivity);
     }
 
     private void FixedUpdate()
